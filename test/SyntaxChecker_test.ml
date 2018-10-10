0e277b97783aa77910c4182d6d625bd367254aa9
logiclogue <jordanpaullord@hotmail.co.uk>
2018-10-10 23:04:58 +0100

    Adds more test cases for syntaxically checking strings, failing

--- test/SyntaxChecker_test.ml
+++ test/SyntaxChecker_test.ml
@@ -1,7 +1,10 @@
 open SyntaxChecker
 
 let test () =
-    assert (check_string "sldfj");
-    assert (check_string "");
+    assert (check_string "\"sldfj\"");
+    assert (check_string "\"\"");
+    assert (not (check_string "\""));
+    assert (not (check_string "alskdjf"));
+    assert (not (check_string ""));
 
     assert (check_int "12");
