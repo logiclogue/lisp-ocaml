bba22aae5de059a57ad36eb6bed0851f70eebbaa
logiclogue <jordanpaullord@hotmail.co.uk>
2018-10-18 09:30:02 +0100

    Replaces use of is digit with char helper one

--- src/SyntaxChecker.ml
+++ src/SyntaxChecker.ml
@@ -11,23 +11,9 @@ let check_string s =
     | (c :: cs) -> c == '"' && after_open_quote cs
 
 let rec check_int s =
-    let check_digit c = match c with
-        | '0' -> true
-        | '1' -> true
-        | '2' -> true
-        | '3' -> true
-        | '4' -> true
-        | '5' -> true
-        | '6' -> true
-        | '7' -> true
-        | '8' -> true
-        | '9' -> true
-        | _   -> false
-        in
-
     match s with
-    | (c :: []) -> check_digit c
-    | (c :: cs) -> check_digit c && check_int cs
+    | (c :: []) -> CharHelpers.is_digit c
+    | (c :: cs) -> CharHelpers.is_digit c && check_int cs
     | []        -> false
 
 let check_name s = false
