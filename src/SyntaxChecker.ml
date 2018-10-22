e25a8237d67ef3759685da3609f360c630d2b56b
logiclogue <jordanpaullord@hotmail.co.uk>
2018-10-22 21:12:15 +0100

    Replaces all uses of match with function

--- src/SyntaxChecker.ml
+++ src/SyntaxChecker.ml
@@ -1,44 +1,41 @@
-let check_string s =
-    let after_escape cs = match cs with
+let check_string =
+    let after_escape = function
         | (c :: cs) -> true
         | []        -> false
         in
 
-    let rec after_quote cs = match cs with
+    let rec after_quote = function
         | ('"' :: [])  -> true
-        | ('\\' :: xs) -> after_escape cs && after_quote cs
+        | ('\\' :: cs) -> after_escape cs && after_quote cs
         | (c :: cs)    -> after_quote cs
         | []           -> false
         in
 
-    match s with
+    function
     | ('"' :: cs) -> after_quote cs
     | _           -> false
 
-let rec check_int s =
-    match s with
+let rec check_int = function
     | (c :: []) -> CharHelpers.is_digit c
     | (c :: cs) -> CharHelpers.is_digit c && check_int cs
     | []        -> false
 
-let check_name s =
+let check_name =
     let is_valid_leading_char c = CharHelpers.is_letter c
         || CharHelpers.is_spacer c in
 
     let is_valid_char c = is_valid_leading_char c || CharHelpers.is_digit c in
 
-    let rec check_rest_of_name cs =
-        match cs with
+    let rec check_rest_of_name = function
         | (c :: cs) -> is_valid_char c && check_rest_of_name cs
         | []        -> true
         in
 
-    match s with
+    function
     | (c :: cs) -> (is_valid_leading_char c) && check_rest_of_name cs
     | []        -> false
 
-let rec list_splitter s =
-    match s with
+let rec list_splitter = function
     | ('(' :: cs) -> "(" :: (list_splitter cs)
     | (')' :: []) -> [")"]
     | (c :: cs)   -> "c" :: (list_splitter cs)
