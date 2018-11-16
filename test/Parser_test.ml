6b360087feeea21875dae3e291eeda14e0e0a314
logiclogue <jordanpaullord@hotmail.co.uk>
2018-11-16 12:48:26 +0000

    Fixes last

--- test/Parser_test.ml
+++ test/Parser_test.ml
@@ -1,6 +1,8 @@
 open Parser
 
 let test () =
+    let char_code = pure Char.code in
+
     assert (parse item "wow" = [('w', "ow")]);
     assert (parse look "wow" = [("wow", "wow")]);
     assert (parse eof "" = [((), "")]);
@@ -8,8 +10,8 @@ let test () =
     assert (parse (Char.code <$> item) "wow" = [(119, "ow")]);
     assert (parse ("$$" <$ item) "wow" = [("$$", "ow")]);
     assert (parse (skip item) "wow" = [((), "ow")]);
-    assert (parse (pure 42) "wow" = [(42, "ow")]);
+    assert (parse (pure 42) "wow" = [(42, "wow")]);
     assert (parse (Char.code <$ item <*> item) "wow" = [(111, "w")]);
     assert (parse (Char.code <$ item <* item <*> item) "wow" = [(119, "")]);
     assert (parse ((Char.code <$ item) *> item) "wow" = [('o', "w")]);
-    assert (parse (item <**> Char.code <*> item) "wow" = [(111, "w")]);
+    assert (parse (item <**> char_code) "wow" = [(119, "ow")]);
